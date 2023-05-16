cb4b1f74bec2672f137a06f578cbf3d179a56835
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-05-16 18:10:37 +0300

    UPDATE

--- BoidsDirectX/BoidsDirectX.cpp
+++ BoidsDirectX/BoidsDirectX.cpp
@@ -118,6 +118,21 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
             else if(wparam == VK_DOWN)   global_keyIsDown[GameActionLookDown]      = isDown;
             else if(wparam == VK_RIGHT)  global_keyIsDown[GameActionTurnCamRight]  = isDown;
             break;
+            // switch (wparam)
+            // {
+            //     case VK_ESCAPE : { DestroyWindow(hwnd);                                break; }
+            //     case 'W'       : { global_keyIsDown[GameActionMoveCamFwd]    = isDown; break; }
+            //     case 'A'       : { global_keyIsDown[GameActionMoveCamLeft]   = isDown; break; }
+            //     case 'S'       : { global_keyIsDown[GameActionMoveCamBack]   = isDown; break; }
+            //     case 'D'       : { global_keyIsDown[GameActionMoveCamRight]  = isDown; break; }
+            //     case 'E'       : { global_keyIsDown[GameActionRaiseCam]      = isDown; break; }
+            //     case 'Q'       : { global_keyIsDown[GameActionLowerCam]      = isDown; break; }
+            //     case VK_UP     : { global_keyIsDown[GameActionLookUp]        = isDown; break; }
+            //     case VK_LEFT   : { global_keyIsDown[GameActionTurnCamLeft]   = isDown; break; }
+            //     case VK_DOWN   : { global_keyIsDown[GameActionLookDown]      = isDown; break; }
+            //     case VK_RIGHT  : { global_keyIsDown[GameActionTurnCamRight]  = isDown; break; }
+            //     default        : {                                                     break; }
+            // }
         }
         case WM_DESTROY:
         {
