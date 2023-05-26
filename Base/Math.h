b9e2c60a6be928849a666473e7b8baa4f36fbd1a
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-05-26 18:01:58 +0300

    UPDATE

--- Base/Math.h
+++ Base/Math.h
@@ -105,6 +105,10 @@ inline float Vector3Length(Vector3 v)
 {
     return sqrtf(v.x*v.x + v.y*v.y + v.z*v.z);
 }
+inline float Vector3Dot(Vector3 a, Vector3 b)
+{
+    return a.x*b.x + a.y*b.y + a.z*b.z;
+}
 inline float Vector4Dot(Vector4 a, Vector4 b)
 {
     return a.x*b.x + a.y*b.y + a.z*b.z + a.w*b.w;
@@ -418,54 +422,19 @@ Matrix MatrixLookAt3(Vector3 eye, Vector3 target, Vector3 up)
 {
     // TODO. Needs tests. Taken from raylib.
 
-    Matrix result = { 0 };
+    Vector3 vz = eye - target;
+    vz = Normalise(vz);
+    Vector3 vx = Cross(up,vz);
+    vx = Normalise(vx);
+    Vector3 vy = Cross(vz,vx);
 
-    float length = 0.0f;
-    float ilength = 0.0f;
-
-    // Vector3Subtract(eye, target)
-    Vector3 vz = { eye.x - target.x, eye.y - target.y, eye.z - target.z };
-
-    // Vector3Normalize(vz)
-    Vector3 v = vz;
-    length = sqrtf(v.x*v.x + v.y*v.y + v.z*v.z);
-    if (length == 0.0f) length = 1.0f;
-    ilength = 1.0f/length;
-    vz.x *= ilength;
-    vz.y *= ilength;
-    vz.z *= ilength;
-
-    // Vector3CrossProduct(up, vz)
-    Vector3 vx = { up.y*vz.z - up.z*vz.y, up.z*vz.x - up.x*vz.z, up.x*vz.y - up.y*vz.x };
-
-    // Vector3Normalize(x)
-    v = vx;
-    length = sqrtf(v.x*v.x + v.y*v.y + v.z*v.z);
-    if (length == 0.0f) length = 1.0f;
-    ilength = 1.0f/length;
-    vx.x *= ilength;
-    vx.y *= ilength;
-    vx.z *= ilength;
-
-    // Vector3CrossProduct(vz, vx)
-    Vector3 vy = { vz.y*vx.z - vz.z*vx.y, vz.z*vx.x - vz.x*vx.z, vz.x*vx.y - vz.y*vx.x };
-
-    result.m0 = vx.x;
-    result.m1 = vy.x;
-    result.m2 = vz.x;
-    result.m3 = 0.0f;
-    result.m4 = vx.y;
-    result.m5 = vy.y;
-    result.m6 = vz.y;
-    result.m7 = 0.0f;
-    result.m8 = vx.z;
-    result.m9 = vy.z;
-    result.m10 = vz.z;
-    result.m11 = 0.0f;
-    result.m12 = -(vx.x*eye.x + vx.y*eye.y + vx.z*eye.z);   // Vector3DotProduct(vx, eye)
-    result.m13 = -(vy.x*eye.x + vy.y*eye.y + vy.z*eye.z);   // Vector3DotProduct(vy, eye)
-    result.m14 = -(vz.x*eye.x + vz.y*eye.y + vz.z*eye.z);   // Vector3DotProduct(vz, eye)
-    result.m15 = 1.0f;
+    return Matrix
+    {
+        vx.x, vx.y, vx.z, -Vector3Dot(vx,eye),
+        vy.x, vy.y, vy.z, -Vector3Dot(vy,eye),
+        vz.x, vz.y, vz.z, -Vector3Dot(vz,eye),
+        0.0f, 0.0f, 0.0f, 1.0f
+    };
 
     return result;
 }
