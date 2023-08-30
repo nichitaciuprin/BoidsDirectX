024438cb44babf1efa9eef3e74c29e84a24607ad
Nichita Ciuprin <nichita.ciuprin@feel-it-services.com>
2023-08-30 13:43:24 +0300

    UPDATE

--- src/Main/Main.cpp
+++ src/Main/Main.cpp
@@ -1,37 +1,96 @@
-#include "../Base/Base.h"
-#include "../Boids/World.h"
-#include "../Renderer/Renderer.h"
+#include <windows.h>
+#include <stdbool.h>
+#include <stdint.h>
 
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
-{
-    UNREFERENCED_PARAMETER(hInstance);
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-    UNREFERENCED_PARAMETER(nCmdShow);
+static bool quit = false;
 
-    auto world = make_unique<World>();
+struct {
+    int width;
+    int height;
+    uint32_t* pixels;
+} frame = {0};
 
-    auto renderer = make_unique<Renderer>(hInstance);
-    renderer->Render(&world->camera, world->boidWorld.boids);
+LRESULT CALLBACK WindowProcessMessage(HWND, UINT, WPARAM, LPARAM);
+#if RAND_MAX == 32767
+#define Rand32() ((rand() << 16) + (rand() << 1) + (rand() & 1))
+#else
+#define Rand32() rand()
+#endif
 
-    while(true)
-    {
-        world->Update(0.02f,
-            renderer->Keydown_VK_LEFT(),
-            renderer->Keydown_VK_UP(),
-            renderer->Keydown_VK_DOWN(),
-            renderer->Keydown_VK_RIGHT(),
-            renderer->Keydown_W(),
-            renderer->Keydown_A(),
-            renderer->Keydown_S(),
-            renderer->Keydown_D(),
-            renderer->Keydown_E(),
-            renderer->Keydown_Q());
+static BITMAPINFO frame_bitmap_info;
+static HBITMAP frame_bitmap = 0;
+static HDC frame_device_context = 0;
 
-        renderer->Render(&world->camera, world->boidWorld.boids);
-        if (renderer->WindowIsClosed())
-            break;
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow) {
+    const wchar_t window_class_name[] = L"My Window Class";
+    static WNDCLASS window_class = { 0 };
+    window_class.lpfnWndProc = WindowProcessMessage;
+    window_class.hInstance = hInstance;
+    window_class.lpszClassName = (PCSTR)window_class_name;
+    RegisterClass(&window_class);
+
+    frame_bitmap_info.bmiHeader.biSize = sizeof(frame_bitmap_info.bmiHeader);
+    frame_bitmap_info.bmiHeader.biPlanes = 1;
+    frame_bitmap_info.bmiHeader.biBitCount = 32;
+    frame_bitmap_info.bmiHeader.biCompression = BI_RGB;
+    frame_device_context = CreateCompatibleDC(0);
+
+    static HWND window_handle;
+    window_handle = CreateWindow((PCSTR)window_class_name, "Drawing Pixels", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                                 640, 300, 640, 480, NULL, NULL, hInstance, NULL);
+    if(window_handle == NULL) { return -1; }
+
+    while(!quit) {
+        static MSG message = { 0 };
+        while(PeekMessage(&message, NULL, 0, 0, PM_REMOVE)) { DispatchMessage(&message); }
+
+        static unsigned int p = 0;
+        frame.pixels[(p++)%(frame.width*frame.height)] = Rand32();
+        frame.pixels[Rand32()%(frame.width*frame.height)] = 0;
+
+        InvalidateRect(window_handle, NULL, FALSE);
+        UpdateWindow(window_handle);
     }
 
+    return 0;
+}
+
+
+LRESULT CALLBACK WindowProcessMessage(HWND window_handle, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch(message) {
+        case WM_QUIT:
+        case WM_DESTROY: {
+            quit = true;
+        } break;
+
+        case WM_PAINT: {
+            static PAINTSTRUCT paint;
+            static HDC device_context;
+            device_context = BeginPaint(window_handle, &paint);
+            BitBlt(device_context,
+                   paint.rcPaint.left, paint.rcPaint.top,
+                   paint.rcPaint.right - paint.rcPaint.left, paint.rcPaint.bottom - paint.rcPaint.top,
+                   frame_device_context,
+                   paint.rcPaint.left, paint.rcPaint.top,
+                   SRCCOPY);
+            EndPaint(window_handle, &paint);
+        } break;
+
+        case WM_SIZE: {
+            frame_bitmap_info.bmiHeader.biWidth  = LOWORD(lParam);
+            frame_bitmap_info.bmiHeader.biHeight = HIWORD(lParam);
+
+            if(frame_bitmap) DeleteObject(frame_bitmap);
+            frame_bitmap = CreateDIBSection(NULL, &frame_bitmap_info, DIB_RGB_COLORS, (void**)&frame.pixels, 0, 0);
+            SelectObject(frame_device_context, frame_bitmap);
+
+            frame.width =  LOWORD(lParam);
+            frame.height = HIWORD(lParam);
+        } break;
+
+        default: {
+            return DefWindowProc(window_handle, message, wParam, lParam);
+        }
+    }
     return 0;
 }
