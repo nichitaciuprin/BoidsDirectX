76208aa6bc3229ef3e8acb0c8c9ca3aa8cc2c157
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-09-03 01:18:33 +0300

    UPDATE

--- src/Main3/Main.cpp
+++ src/Main3/Main.cpp
@@ -10,6 +10,79 @@ void ChangePixelsRandomly(uint32_t* pixels, size_t width, size_t height, size_t 
     pixels[i1] = Subgen::StaticNext();
     pixels[i2] = 0;
 }
+typedef int (__cdecl *MYPROC)(LPCWSTR);
+
+// int main( void )
+// {
+//     HINSTANCE hinstLib;
+//     MYPROC ProcAdd;
+//     BOOL fFreeResult, fRunTimeLinkSuccess = FALSE;
+
+//     // Get a handle to the DLL module.
+
+//     hinstLib = LoadLibrary(TEXT("MyPuts.dll"));
+
+//     // If the handle is valid, try to get the function address.
+
+//     if (hinstLib != NULL)
+//     {
+//         ProcAdd = (MYPROC) GetProcAddress(hinstLib, "myPuts");
+
+//         // If the function address is valid, call the function.
+
+//         if (NULL != ProcAdd)
+//         {
+//             fRunTimeLinkSuccess = TRUE;
+//             (ProcAdd) (L"Message sent to the DLL function\n");
+//         }
+//         // Free the DLL module.
+
+//         fFreeResult = FreeLibrary(hinstLib);
+//     }
+
+//     // If unable to call the DLL function, use an alternative.
+//     if (! fRunTimeLinkSuccess)
+//         printf("Message printed from executable\n");
+
+//     return 0;
+
+// }
+void DrawSquare(uint32_t* pixels, size_t width)
+{
+    for (size_t row    = 0; row    < 20; row++)
+    for (size_t column = 0; column < 20; column++)
+    {
+        pixels[column+row*width] = ~(uint32_t)0;
+    }
+}
+// void Line(uint32_t* pixels, size_t width, int x0, int y0, int x1, int y1)
+// {
+//     int dx = abs(x1 - x0);
+//     int dy = abs(y1 - y0);
+//     int sx = x0 < x1 ? 1 : -1;
+//     int sy = y0 < y1 ? 1 : -1;
+//     int err = (dx > dy ? dx : -dy) / 2;
+//     int e2;
+
+//     while (true)
+//     {
+//         // setPixel(x0,y0);
+//         if (x0 == x1 && y0 == y1) break;
+//         e2 = err;
+//         if (e2 > -dx) { err -= dy; x0 += sx; }
+//         if (e2 <  dy) { err += dx; y0 += sy; }
+//     }
+// }
+// void Duno(uint32_t* pixels, size_t width, size_t x1, size_t y1, size_t x2, size_t y2)
+// {
+//     auto dx = x2 - x1;
+//     auto dy = y2 - y1;
+//     for (size_t i = 0; i < dx; i++)
+//     {
+//         auto y = y1 + dy * ((x1+i) - x1) / dx;
+//         plot(x, y);
+//     }
+// }
 
 int main()
 {
@@ -27,6 +100,8 @@ int main()
         size_t height;
         BitmapWindow::GetBitmapInfo(&width, &height);
         ChangePixelsRandomly(pixels, width, height, pixelIndex);
+        DrawSquare(pixels, width);
+        // Line(0,0,10,10);
         BitmapWindow::SetPixels(pixels);
         pixelIndex++;
 
