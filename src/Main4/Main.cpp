53f7a9965b917690727c28e3db3db084155c2515
Nichita Ciuprin <nichita.ciuprin@feel-it-services.com>
2023-09-06 17:09:11 +0300

    UPDATE

--- src/Main4/Main.cpp
+++ src/Main4/Main.cpp
@@ -20,11 +20,9 @@ int main()
         auto t1 = MathSin(time);
         auto t2 = MathCos(time);
         Vector3 direction = {};
-        // direction += Vector3Forward();
         direction += Vector3{t1,0,t2};
         direction += Vector3{t1/2,t2/2,0};
         direction = Vector3Normalize(direction);
-        // auto direction = Vector3Forward()+Vector3Up()+Vector3Right();
         bitmap->DrawCube(Vector3Zero(),direction);
 
         bitmap->DrawBorder(GREEN);
