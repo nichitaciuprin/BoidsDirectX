822b71f9219612db474e3901fd848e6548ede328
Nichita Ciuprin <nichita.ciuprin@feel-it-services.com>
2023-09-06 11:09:41 +0300

    UPDATE

--- src/Main4/Main.cpp
+++ src/Main4/Main.cpp
@@ -13,26 +13,19 @@ int main()
     while (BitmapWindow::Exists())
     {
         CheckFPS();
-        bitmap->DrawX(RED);
+        // bitmap->DrawX(RED);
         // bitmap->DrawCross(BLUE);
-        bitmap->DrawBorder(GREEN);
 
-        uint32_t outX0, outY0;
-        uint32_t outX1, outY1;
-        Vector2 v0 = {};
-        Vector2 v1 = {};
-        v1.x = 1;
-        v1.y = 1;
+        uint32_t outX0, outY0; Vector2 v0 = {};
+        uint32_t outX1, outY1; Vector2 v1 = {};
+        v1.x = 1; v1.y = 1;
         bitmap->ToScreenSpace(v0,&outX0,&outY0);
         bitmap->ToScreenSpace(v1,&outX1,&outY1);
-        // cout << outX1 << endl;
-        // cout << outY1 << endl;
-        // outX0 -= 1;
-        // outY0 -= 1;
 
         bitmap->DrawLine(outX0,outY0,outX1,outY1,BLUE);
         // bitmap->DrawLine(150,150,200,150,BLUE);
 
+        bitmap->DrawBorder(GREEN);
         BitmapWindow::SetPixels2(bitmap);
         BitmapWindow::Update();
     }
