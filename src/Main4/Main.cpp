ad38cc35d539e6dde5127c44578500364358fef3
Nichita Ciuprin <nichita.ciuprin@feel-it-services.com>
2023-09-07 11:51:18 +0300

    UPDATE

--- src/Main4/Main.cpp
+++ src/Main4/Main.cpp
@@ -16,14 +16,14 @@ int main()
         bitmap->Clear(BLACK);
 
         auto position = Vector3Zero();
-        auto time = (float)clock()/400;
+        auto time = (float)clock()/800;
         auto t1 = MathSin(time);
         auto t2 = MathCos(time);
         Vector3 direction = {};
         direction += Vector3{t1,0,t2};
         direction += Vector3{t1/2,t2/2,0};
         direction = Vector3Normalize(direction);
-        bitmap->DrawCube(Vector3Zero(),direction);
+        bitmap->DrawCube(position,direction);
 
         bitmap->DrawBorder(GREEN);
         BitmapWindow::SetPixels2(bitmap);
