28ffcb76510f7efe02727580efbc5aef2012d832
Nichita Ciuprin <nichita.ciuprin@feel-it-services.com>
2023-10-16 10:24:32 +0300

    UPDATE

--- src/Main7/Main.cpp
+++ src/Main7/Main.cpp
@@ -2,30 +2,24 @@
 
 void main2()
 {
-    Vector3 from = { 1, 2, 0 };
-    Vector3 dir = { 0, 1, 0 };
-    dir = Vector3Normalize(dir);
-    Sphere sphere = { { 0, 0, 0 }, 4.0f };
-    float outDistance = 0;
-    Vector3 outPoint = {};
-    Vector3 outNormal = {};
-    auto isHit = Raycast3(from, dir, sphere, &outDistance, &outPoint, &outNormal);
-    // cout << isHit << endl;
-    // cout << outDistance << endl;
-    // Vector3Print(outPoint);
+    auto width = 400;
+    auto height = 400;
 
-    // Vector3 origin = { 0, 0, 0 };
-    // Vector3 dirNorm = { 0, 0, 1 };
-    // Sphere sphere = { { 0, 0, -20 }, 10.0f };
-    // dirNorm = Vector3Normalize(dirNorm);
-    // auto isHit = Raycast(origin, dirNorm, sphere);
-    // cout << isHit << endl;
+    auto bitmap = make_unique<Bitmap>(width, height);
 
-    // Vector3 start = { 0, 0, 0 };
-    // Vector3 end = { 0, 0, 100.0f };
-    // Sphere sphere = { { 0.0f, 0, 102 }, 1.0f };
-    // auto isHit = RaycastLine(start, end, sphere);
-    // cout << isHit << endl;
+    BitmapWindow1::Create(700, 100, width, height);
+
+    while (BitmapWindow1::Exists())
+    {
+        CheckFPS();
+        bitmap->Fill(BLACK);
+
+        // TODO
+
+        bitmap->DrawBorder(GREEN);
+        BitmapWindow1::SetPixels(bitmap);
+        BitmapWindow1::Update();
+    }
 }
 
 int main()
